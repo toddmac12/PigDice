1fe3ac81ce4f4ad5ec8a10feca92129524611aa6
Charlie Weber <charlestweber@gmail.com>
2021-08-26 13:48:30 -0700

    it will adjust formatted and remove console logs

--- js/scripts.js
+++ js/scripts.js
@@ -10,108 +10,92 @@ function Game() {
 
 // true turn value = player 2 --------  flase turn value = player 1
 
-Game.prototype.roll = function(){
-// let diceValue = 0
-let diceValue = Math.floor((Math.random()*6 ) + 1) 
-if (diceValue === 1){
-  this.activeScore = 0
-  this.dice = diceValue
-}
-else{
-this.activeScore += diceValue
-// this.winCheck();
-this.dice = diceValue
-}
-// console.log(diceValue)
-// console.log(this.dice)
-// console.log(this.activeScore)
+Game.prototype.roll = function () {
+  let diceValue = Math.floor((Math.random() * 6) + 1)
+  if (diceValue === 1) {
+    this.activeScore = 0
+    this.dice = diceValue
+  }
+  else {
+    this.activeScore += diceValue
+    this.dice = diceValue
+  }
 };
 
-Game.prototype.changeTurn = function(){
-  if (game.turn){
+Game.prototype.changeTurn = function () {
+  if (game.turn) {
     this.turn = false
     this.activePlayer = "player1"
   }
-  else{
+  else {
     this.turn = true
     this.activePlayer = "player2"
   }
-  // console.log(game.turn)
 };
 
-Game.prototype.hold = function(){
-if (this.turn === false){
-  this.score.player1 += this.activeScore
-  this.activeScore = 0
-  this.changeTurn();
-  // console.log(this.activePlayer)
-  // return this.score.player1
-  // return not necessary
-} 
-else{
-  this.score.player2 += this.activeScore
-  this.activeScore = 0
-  this.changeTurn();
-  // console.log(this.activePlayer)
-  // return this.score.player2
-}
+Game.prototype.hold = function () {
+  if (this.turn === false) {
+    this.score.player1 += this.activeScore
+    this.activeScore = 0
+    this.changeTurn();
+  }
+  else {
+    this.score.player2 += this.activeScore
+    this.activeScore = 0
+    this.changeTurn();
+  }
 
 };
 
-Game.prototype.winCheck = function(){
-  if (this.score[this.activePlayer] + this.activeScore >= 50){
+Game.prototype.winCheck = function () {
+  if (this.score[this.activePlayer] + this.activeScore >= 50) {
     console.log(this.activePlayer + "wins")
     return "win"
   }
-  else{
+  else {
     console.log("game continues")
     return "continue"
   }
-  }
+}
 
 // USER INTERFACE LOGIC
 let game = new Game
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("span#dice-value").text(game.dice)
   $("span#active-score").text(game.activeScore)
   $("span#active-player").text(game.activePlayer)
 
-  $("button#roll").click(function(){
+  $("button#roll").click(function () {
     let dice = game.roll();
     let winCondition = game.winCheck();
     $("span#dice-value").text(game.dice);
     $("span#active-score").text(game.activeScore);
-    $("span#" + game.activePlayer +"-held-score").text(game.score[game.activePlayer] + game.activeScore);
-    if (winCondition === "win"){
+    $("span#" + game.activePlayer + "-held-score").text(game.score[game.activePlayer] + game.activeScore);
+    if (winCondition === "win") {
       $("div#win-screen").show();
       $("div#game-board").hide();
       $("div#buttons").hide();
       $("span#winnerName").text(game.activePlayer);
     }
     else {
-      if (game.turn === true){
+      if (game.turn === true) {
         $("player2div").show();
         $("player1div").hide();
       }
-      else{
+      else {
         $("player2div").hide();
         $("player1div").show();
       }
-  }
-  // $("span#active-player").text(game.activePlayer)
-  console.log(game)
+    }
   })
 
-  $("button#hold").click(function(){
-    // $("span#" + game.activePlayer +"-total-score").text(game.activeScore += game.score[game.activePlayer] );
-    // $("span#"+ game.activePlayer +"-held-score").text(0)
-    // console.log($("span#"+ game.activePlayer +"-held-score").text())
+  $("button#hold").click(function () {
     game.hold();
     $("span#dice-value").text(0);
     $("span#active-score").text(game.activeScore);
     console.log(game.turn)
-    if (game.turn === true){
+    if (game.turn === true) {
       $("span#player2-held-score").text(0);
       $("span#player1-held-score").text(0);
       $("span#player1-total-score").text(game.score.player1);
@@ -119,7 +103,7 @@ $(document).ready(function(){
       $("player2div").show();
       $("player1div").hide();
     }
-    else{
+    else {
       $("span#player2-held-score").text(0);
       $("span#player1-held-score").text(0);
       $("span#player1-total-score").text(game.score.player1);
